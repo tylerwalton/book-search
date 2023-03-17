1c8b07aa7d338eb5813abe892f4ac47dfc2a811d
Tyler Walton <tylerwalton300@gmail.com>
2023-03-17 18:39:50 -0400

    adjusted auth.js to work for graphql

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,55 @@
-const jwt = require('jsonwebtoken');
+const { ApolloServer, AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+// function for our authenticated routes
+const authMiddleware = (context) => {
+  // allows token to be sent via headers or query parameters
+  let token = context.req.headers.authorization || "";
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  if (token.startsWith("Bearer ")) {
+    // Remove "Bearer " from token string
+    token = token.slice(7, token.length);
+  }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+  if (!token) {
+    throw new AuthenticationError("You must be logged in");
+  }
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    context.user = data;
+  } catch (err) {
+    console.log(err);
+    throw new AuthenticationError("Invalid token");
+  }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  return context;
+};
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
   },
 };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    return authMiddleware({ req });
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
